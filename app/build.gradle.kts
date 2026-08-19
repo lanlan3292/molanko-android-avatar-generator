94611ec88aeca099af85406d69c42e03ecbc3e86
lanlan3292 <149509569+lanlan3292@users.noreply.github.com>
2026-08-20 02:03:03 +0800

    1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         resourceConfigurations.addAll(listOf("zh", "en", "zh-rCN", "zh-rTW"))
     }
